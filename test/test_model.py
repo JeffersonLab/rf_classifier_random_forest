6593bec7920e402d70f0eb6b58a55a9a6d9cfbd4
Adam Carpenter <adamc@jlab.org>
2020-01-09 09:58:54 -0500

    Update results for new models, only check conf vals out to two decimals

--- test/test_model.py
+++ test/test_model.py
@@ -17,8 +17,8 @@ exp = {
             'result': {
                 'cavity-label': '2',
                 'fault-label': 'Microphonics',
-                'cavity-confidence': 0.90,
-                'fault-confidence': 0.7825079365079365,
+                'cavity-confidence': 0.69,
+                'fault-confidence': 0.79,
                 'location': '0L04',
                 'timestamp': '2018-04-26 07:07:34.9'
             }
@@ -28,10 +28,10 @@ exp = {
         {
             'path': os.path.join(os.path.dirname(__file__), 'test-data', '1L22', '2018_05_02', '122116.5'),
             'result': {
-                'cavity-label': '5',
-                'fault-label': 'Quench',
-                'cavity-confidence': 0.8833333333333333,
-                'fault-confidence': 0.4421483331483332,
+                'cavity-label': '7',  # Model gets this wrong
+                'fault-label': 'Quench', # Model gets this wrong
+                'cavity-confidence': 0.29,
+                'fault-confidence': 0.74,
                 'location': '1L22',
                 'timestamp': '2018-05-02 12:21:16.5'
             }
@@ -39,10 +39,10 @@ exp = {
         {
             'path': os.path.join(os.path.dirname(__file__), 'test-data', '1L22', '2018_05_02', '132811.9'),
             'result': {
-                'cavity-label': '5',
+                'cavity-label': '7', # Model gets this wrong
                 'fault-label': 'Quench',
-                'cavity-confidence': 0.7796296296296297,
-                'fault-confidence': 0.6147303437303439,
+                'cavity-confidence': 0.27,
+                'fault-confidence': 0.71,
                 'location': '1L22',
                 'timestamp': '2018-05-02 13:28:11.9'
             }
@@ -54,8 +54,8 @@ exp = {
             'result': {
                 'cavity-label': '2',
                 'fault-label': 'Single Cav Turn off',
-                'cavity-confidence': 0.9833333333333333,
-                'fault-confidence': 0.8466666666666667,
+                'cavity-confidence': 0.88,
+                'fault-confidence': 1.00,
                 'location': '1L23',
                 'timestamp': '2018-05-02 15:27:47.4'
             }
@@ -68,7 +68,7 @@ exp = {
                 'cavity-label': '6',
                 'fault-label': 'Quench',
                 'cavity-confidence': 1.00,
-                'fault-confidence': 0.88,
+                'fault-confidence': 1.00,
                 'location': '1L24',
                 'timestamp': '2018-05-03 07:48:36.5'
             }
@@ -78,8 +78,8 @@ exp = {
             'result': {
                 'cavity-label': '6',
                 'fault-label': 'Quench',
-                'cavity-confidence': 1.00,
-                'fault-confidence': 0.88,
+                'cavity-confidence': 0.99,
+                'fault-confidence': 0.99,
                 'location': '1L24',
                 'timestamp': '2018-05-04 04:48:22.5'
             }
@@ -91,12 +91,24 @@ exp = {
             'result': {
                 'cavity-label': '4',
                 'fault-label': 'Microphonics',
-                'cavity-confidence': 0.9142515980751273,
-                'fault-confidence': 0.9053333333333334,
+                'cavity-confidence': 0.81,
+                'fault-confidence': 0.87,
                 'location': '1L25',
                 'timestamp': '2018-04-26 16:13:22.6'
             }
-        }
+        },
+        # Skip this one since it fails the 'all cavities either bypassed or in GDR mode' test
+        # {
+        #     'path': os.path.join(os.path.dirname(__file__), 'test-data', '1L25', '2018_04_29', '221542.6'),
+        #     'result': {
+        #         'cavity-label': '1',
+        #         'fault-label': 'Quench',
+        #         'cavity-confidence': 0.75,
+        #         'fault-confidence': 0.83,
+        #         'location': '1L25',
+        #         'timestamp': '2018-04-29 22:15:42.6'
+        #     }
+        # }
     ],
     '1L26': [
         {
@@ -104,8 +116,8 @@ exp = {
             'result': {
                 'cavity-label': '1',
                 'fault-label': 'E_Quench',
-                'cavity-confidence': 0.9333333333333333,
-                'fault-confidence': 1.00,
+                'cavity-confidence': 0.89,
+                'fault-confidence': 0.92,
                 'location': '1L26',
                 'timestamp': '2018-04-27 14:39:18.4'
             }
@@ -115,22 +127,30 @@ exp = {
             'result': {
                 'cavity-label': '8',
                 'fault-label': 'Single Cav Turn off',
-                'cavity-confidence': 0.9833333333333333,
-                'fault-confidence': 0.6602247382247383,
+                'cavity-confidence': 0.69,
+                'fault-confidence': 1.00,
                 'location': '1L26',
                 'timestamp': '2018-04-29 19:34:09.3'
             }
         },
-        # Chris said this should produced a div by zero error during feature extraction - but he was using
-        # a development version of tsfresh to get around a different bug.  Seems OK here, but I don't have
-        # his numbers to compare against.  Just use the values output by this model and check for future
-        # deviations.
+        # Skip this one since it fails the 'all cavities either bypassed or in GDR mode' test
+        # {
+        #     'path': os.path.join(os.path.dirname(__file__), 'test-data', '1L26', '2018_05_02', '111441.1'),
+        #     'result': {
+        #         'cavity-label': '8',
+        #         'fault-label': 'E_Quench',
+        #         'cavity-confidence': 0.91,
+        #         'fault-confidence': 0.89,
+        #         'location': '1L26',
+        #         'timestamp': '2018-05-02 11:14:41.1'
+        #     }
+        # },
         {
             'path': os.path.join(os.path.dirname(__file__), 'test-data', '1L26', '2018_05_03', '144259.6'),
             'result': {
                 'cavity-label': '8',
                 'fault-label': 'E_Quench',
-                'cavity-confidence': 0.9333333333333333,
+                'cavity-confidence': 0.85,
                 'fault-confidence': 1.0,
                 'location': '1L26',
                 'timestamp': '2018-05-03 14:42:59.6'
@@ -141,8 +161,8 @@ exp = {
             'result': {
                 'cavity-label': '6',
                 'fault-label': 'Microphonics',
-                'cavity-confidence': 0.8375,
-                'fault-confidence': 0.6704021164021164,
+                'cavity-confidence': 0.92,
+                'fault-confidence': 0.50,
                 'location': '1L26',
                 'timestamp': '2018-05-04 08:54:59.2'
             }
@@ -152,8 +172,8 @@ exp = {
             'result': {
                 'cavity-label': '1',
                 'fault-label': 'E_Quench',
-                'cavity-confidence': 0.8333333333333334,
-                'fault-confidence': 0.9733333333333334,
+                'cavity-confidence': 0.85,
+                'fault-confidence': 0.71,
                 'location': '1L26',
                 'timestamp': '2018-05-05 18:15:45.5'
             }
@@ -163,8 +183,8 @@ exp = {
             'result': {
                 'cavity-label': '1',
                 'fault-label': 'Quench',  # Model predicts the wrong fault.  This is that "wrong" prediction.
-                'cavity-confidence': 0.9875,
-                'fault-confidence': 0.7817777777777777,
+                'cavity-confidence': 0.85,
+                'fault-confidence': 0.57,
                 'location': '1L26',
                 'timestamp': '2018-05-06 00:27:24.6'
             }
@@ -174,10 +194,10 @@ exp = {
         {
             'path': os.path.join(os.path.dirname(__file__), 'test-data', '2L22', '2018_05_03', '081145.9'),
             'result': {
-                'cavity-label': '7',  # Model get's it wrong.  This is the expected prediction.
-                'fault-label': 'Single Cav Turn off',  # Model get's it wrong.  This is the expected "wrong" prediction.
-                'cavity-confidence': 0.48333333333333334,
-                'fault-confidence': 0.46882450882450893,
+                'cavity-label': '8',
+                'fault-label': 'Quench',
+                'cavity-confidence': 0.60,
+                'fault-confidence': 0.64,
                 'location': '2L22',
                 'timestamp': '2018-05-03 08:11:45.9'
             }
@@ -189,8 +209,8 @@ exp = {
             'result': {
                 'cavity-label': '1',
                 'fault-label': 'E_Quench',
-                'cavity-confidence': 0.9128205128205128,
-                'fault-confidence': 0.9361904761904764,
+                'cavity-confidence': 0.87,
+                'fault-confidence': 0.80,
                 'location': '2L23',
                 'timestamp': '2018-05-05 09:01:20.3'
             }
@@ -202,8 +222,8 @@ exp = {
             'result': {
                 'cavity-label': '3',
                 'fault-label': 'Single Cav Turn off',
-                'cavity-confidence': 0.8531746031746033,
-                'fault-confidence': 0.86,
+                'cavity-confidence': 0.75,
+                'fault-confidence': 1.00,
                 'location': '2L24',
                 'timestamp': '2018-04-25 13:08:53.9'
             }
@@ -211,10 +231,10 @@ exp = {
         {
             'path': os.path.join(os.path.dirname(__file__), 'test-data', '2L24', '2018_04_26', '142849.9'),
             'result': {
-                'cavity-label': '5',
-                'fault-label': 'Microphonics',
-                'cavity-confidence': 0.6553885158051824,
-                'fault-confidence': 0.5162380952380953,
+                'cavity-label': 'multiple', # Model gets this wrong
+                'fault-label': 'Multi Cav Turn off', # Hence, model also gets this wrong
+                'cavity-confidence': 0.24,
+                'fault-confidence': 0.24,
                 'location': '2L24',
                 'timestamp': '2018-04-26 14:28:49.9'
             }
@@ -224,8 +244,8 @@ exp = {
             'result': {
                 'cavity-label': '4',
                 'fault-label': 'Single Cav Turn off',
-                'cavity-confidence': 0.48253968253968255,
-                'fault-confidence': 0.86,
+                'cavity-confidence': 0.50,
+                'fault-confidence': 0.98,
                 'location': '2L24',
                 'timestamp': '2018-04-27 02:10:43.7'
             }
@@ -257,6 +277,8 @@ class TestRandomForest(TestCase):
                     print(path)
                     mod = Model(path)
                     result = mod.analyze(deployment='history')
+                    result['cavity-confidence'] = round(result['cavity-confidence'], 2)
+                    result['fault-confidence'] = round(result['fault-confidence'], 2)
                     try:
                         self.assertDictEqual(expect, result)
                     except Exception as e:
