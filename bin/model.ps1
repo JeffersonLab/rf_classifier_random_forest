177d500c4b48442c26e16af80f089d5b385064bb
Adam Carpenter <adamc@jlab.org>
2020-02-25 14:13:54 -0500

    Added check for venv existence

--- bin/model.ps1
+++ bin/model.ps1
@@ -3,13 +3,20 @@ $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
 $appDir = split-path -parent $scriptDir
 
 # Activate the apps python environment.
-& ${appDir}\venv\Scripts\Activate.ps1
-if (! $?) {
-    echo "Error activating python virtual environment.  Exiting."
-    exit 1
+if (Test-Path ${appDir}\venv)
+{
+    & ${appDir}\venv\Scripts\Activate.ps1
+    if (!$?)
+    {
+        echo "Error activating python virtual environment.  Exiting."
+        exit 1
+    }
 }
 
 # Run the app passing along all of the args
 python.exe ${appDIR}/lib/model.py $args
 
-deactivate
+if (Test-Path ${appDir}\venv)
+{
+    deactivate
+}
