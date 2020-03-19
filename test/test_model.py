ae4d6404b25dc3caef7151f56a378f5058f0f4b9
Adam Carpenter <adamc@jlab.org>
2020-03-19 11:20:56 -0400

    Add support for tests that should throw, disable np warnigns, clean up

--- test/test_model.py
+++ test/test_model.py
@@ -20,14 +20,14 @@ class TestRandomForest(TestCase):
         # Context manager resets warnings filters to default after code section exits
         # I've spent tons of time trying to figure out why the analyze method and specifically it call of
         # sklearn.externals.joblib.load(...) produces RuntimeWarnings for numpy.ufunc binary incompatibility _ONLY_ when
-        # running from unittest.  I give up.  Just suppress these warnings .
+        # running from unittest.  I give up.  Just suppress these warnings.
         with warnings.catch_warnings():
-            # warnings.filterwarnings("ignore", "can't resolve package from __spec__ or __package__, "
-            #                                   "falling back on __name__ and __path__")
-            # warnings.filterwarnings("ignore", "numpy.ufunc size changed, may indicate binary incompatibility. "
-            #                                   "Expected 192 from C header, got 216 from PyObject")
-            # warnings.filterwarnings("ignore", "numpy.ufunc size changed, may indicate binary incompatibility. "
-            #                                   "Expected 216, got 192")
+            warnings.filterwarnings("ignore", "can't resolve package from __spec__ or __package__, "
+                                              "falling back on __name__ and __path__")
+            warnings.filterwarnings("ignore", "numpy.ufunc size changed, may indicate binary incompatibility. "
+                                              "Expected 192 from C header, got 216 from PyObject")
+            warnings.filterwarnings("ignore", "numpy.ufunc size changed, may indicate binary incompatibility. "
+                                              "Expected 216, got 192")
             failed = 0
             test_set = testing_utils.TestSet('test_set.txt')
             for test_event in test_set.get_events():
@@ -47,28 +47,43 @@ class TestRandomForest(TestCase):
                 path = event.event_dir
 
                 mod = Model(path)
-                try:
-                    # The history archiver has everything, except recent data.  ops archiver has recent data, but not
-                    # anything more than maybe two years old.
-                    deployment = 'history'
-                    if datetime.datetime.now() - event.timestamp < datetime.timedelta(days=31):
-                        deployment = 'ops'
-                    result = mod.analyze(deployment=deployment)
-                except Exception as e:
-                    failed += 1
-                    print("Error analyzing data")
-                    print(e)
-                    continue
 
-                # The test set file only has four decimal places.
-                result['cavity-confidence'] = round(result['cavity-confidence'], 4)
-                result['fault-confidence'] = round(result['fault-confidence'], 4)
+                # The history archiver has everything, except recent data.  ops archiver has recent data, but not
+                # anything more than maybe two years old.
+                deployment = 'history'
+                if datetime.datetime.now() - event.timestamp < datetime.timedelta(days=91):
+                    deployment = 'ops'
 
-                try:
-                    self.assertDictEqual(expect, result)
-                except Exception as e:
-                    failed += 1
-                    print(e)
+                # Some test cases are intended to trigger exceptions, handle them differently than the "good" cases
+                if expect['throws']:
+                    try:
+                        mod.analyze(deployment=deployment)
+                    except:
+                        pass
+                    else:
+                        failed += 1
+                        print("FAIL: Model should have thrown exception, but did not")
+
+                else:
+                    try:
+                        result = mod.analyze(deployment=deployment)
+                    except Exception as e:
+                        failed += 1
+                        print("Error analyzing data")
+                        print(e)
+                        continue
+
+                    # The test set file only has four decimal places.
+                    result['cavity-confidence'] = round(result['cavity-confidence'], 4)
+                    result['fault-confidence'] = round(result['fault-confidence'], 4)
+
+                    # Remove the throws entry from expected since the result won't have this
+                    del expect['throws']
+                    try:
+                        self.assertDictEqual(expect, result)
+                    except Exception as e:
+                        failed += 1
+                        print(e)
 
                 try:
                     event.delete_event_data()
@@ -77,6 +92,9 @@ class TestRandomForest(TestCase):
                     print(e)
 
             if failed > 0:
+                print("\n############### FAIL ###################")
+                print("Failed {} example tests".format(failed))
+                print("############### FAIL ###################")
                 self.fail("Failed {} example tests".format(failed))
 
 
